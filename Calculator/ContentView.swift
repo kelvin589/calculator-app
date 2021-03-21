59ff223ebfcd9f2624c8796f6f1423e2e4f104ed
k123459 <190081735@aston.ac.uk>
2021-03-21 19:10:14 +0000

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Kelvin on 21/03/2021.
+//  Copyright © 2021 Kelvin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
