f9ba643598da7950ab895000035603df5f7f3297
kelvin589 <190081735@aston.ac.uk>
2021-03-31 13:39:26 +0100

    Add basic calculator functionality
    Functionality for calculator to add, subtract, multiply and divide.
    Equals must be pressed after every operation.
    Equals can be pressed multiple times to repeat the previous operation.
    Clear all to erase state from previous calculations.

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -9,8 +9,97 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Operation: String, CaseIterable {
+        case adddition = "+"
+        case subtraction = "-"
+        case multiplication = "*"
+        case division = "/"
+    }
+    
+    @State private var result = ""
+    @State private var tempNumber = ""
+    @State private var previousNumber = ""
+    @State private var calculated = false
+    @State private var currentOperation = Operation.adddition
+    @State private var operationButtonPressed = false
+    
+    func operationPressed(_ op: Operation) {
+        self.currentOperation = op
+    }
+    
+    func equalsPressed() {
+        let num1 = Float(previousNumber) ?? 0
+        let num2 = Float(tempNumber) ?? 0
+        
+        switch currentOperation {
+            case .adddition:
+                result = String(num1 + num2)
+            case .subtraction:
+                result = String(num1 - num2)
+            case .multiplication:
+                result = String(num1 * num2)
+            case .division:
+                result = String(num1 / num2)
+        }
+        
+        previousNumber = result
+        calculated = true
+        
+        debugPrint("Actual calculation: \(num1) \(currentOperation.rawValue) \(num2)")
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                HStack {
+                    ForEach(0..<10) { number in
+                        Button("\(number)") {
+                            if (self.operationButtonPressed || self.result == "0") {
+                                self.previousNumber = self.tempNumber
+                                self.tempNumber = String(number)
+                                self.operationButtonPressed = false
+                            } else {
+                                self.tempNumber += String(number)
+                            }
+                            self.result = self.tempNumber
+                        }
+                    }
+                    Button(".") {
+                        self.tempNumber += "."
+                    }
+                }
+                
+                HStack {
+                    ForEach(Operation.allCases, id: \.self) { operation in
+                        Button("\(operation.rawValue)") {
+                            self.operationButtonPressed = true
+                            if (self.calculated) {
+                                self.tempNumber = self.previousNumber
+                                self.calculated = false
+                            }
+                            self.operationPressed(operation)
+                        }
+                    }
+                    Button("=") {
+                        self.operationButtonPressed = true
+                        self.equalsPressed()
+                    }
+                }
+                
+                Text("\(result)")
+                Text("Num1: \(previousNumber)")
+                Text("Operator: \(currentOperation.rawValue)")
+                Text("Num2: \(tempNumber)")
+                
+                Button("Clear All") {
+                    self.result = ""
+                    self.tempNumber = ""
+                    self.previousNumber = ""
+                    self.calculated = false
+                }
+            }
+            .navigationBarTitle("Calculator")
+        }
     }
 }
 
